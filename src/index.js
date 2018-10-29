a36d0468b6b5a66aee0121606d594d02c5b96275
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2018-10-29 14:12:19 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+/* ДЗ 6.1 - Асинхронность и работа с сетью */
+
+/**
+ * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ *
+ * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
+ * @return {Promise}
+ */
+function delayPromise(seconds) {
+}
+
+/**
+ * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
+ * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ * Элементы полученного массива должны быть отсортированы по имени города
+ *
+ * @return {Promise<Array<{name: String}>>}
+ */
+function loadAndSortTowns() {
+}
+
+export {
+    delayPromise,
+    loadAndSortTowns
+};
